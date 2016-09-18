bf2b8101475038b70d2beec1bcebf0f4ab41d142
dkmeena <dmdineshdinu6@gmail.com>
2016-09-18 17:34:15 +0530

    partial sync

--- app/src/main/java/com/example/dinesh/sherlockx/MainActivity.java
+++ app/src/main/java/com/example/dinesh/sherlockx/MainActivity.java
@@ -34,14 +34,17 @@ import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.text.SimpleDateFormat;
@@ -238,7 +241,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
                 try{
                     URL url = new URL("http://10.129.28.209:8080/sherlock_server/Main");
-                    URLConnection connection = url.openConnection();
+                    HttpURLConnection conn = (HttpURLConnection)url.openConnection();
 
 
                     File dir = getExternalFilesDir(null);
@@ -247,6 +250,109 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     for(File f : file){
                         Log.d("inputString", String.valueOf(f));
 
+                        FileInputStream fstrm = new FileInputStream(f);
+
+                        String iFileName = "ovicam_temp_vid.mp4";
+                        String lineEnd = "\r\n";
+                        String twoHyphens = "--";
+                        String boundary = "*****";
+                        String Tag="fSnd";
+                        Log.e(Tag,"Starting Http File Sending to URL");
+
+                        // Open a HTTP connection to the URL
+                       // HttpURLConnection conn = (HttpURLConnection)connectURL.openConnection();
+
+                        // Allow Inputs
+                        conn.setDoInput(true);
+
+                        // Allow Outputs
+                        conn.setDoOutput(true);
+
+                        // Don't use a cached copy.
+                        conn.setUseCaches(false);
+
+                        // Use a post method.
+                        conn.setRequestMethod("POST");
+
+                        conn.setRequestProperty("Connection", "Keep-Alive");
+
+                        conn.setRequestProperty("Content-Type", "multipart/form-data;boundary="+boundary);Log.e(Tag,"Starting Http File Sending to URL");
+
+                        // Open a HTTP connection to the URL
+                        //HttpURLConnection conn = (HttpURLConnection)connectURL.openConnection();
+
+
+                        // Allow Inputs
+                        conn.setDoInput(true);
+
+                        // Allow Outputs
+                        conn.setDoOutput(true);
+
+                        // Don't use a cached copy.
+                        conn.setUseCaches(false);
+
+                        // Use a post method.
+                        conn.setRequestMethod("POST");
+
+                        conn.setRequestProperty("Connection", "Keep-Alive");
+
+                        conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+                        DataOutputStream dos = new DataOutputStream(conn.getOutputStream());
+
+                        dos.writeBytes(twoHyphens + boundary + lineEnd);
+                        dos.writeBytes("Content-Disposition: form-data; name=\"title\""+ lineEnd);
+                        dos.writeBytes(lineEnd);
+                        dos.writeBytes("Title");
+                        dos.writeBytes(lineEnd);
+                        dos.writeBytes(twoHyphens + boundary + lineEnd);
+
+                        dos.writeBytes("Content-Disposition: form-data; name=\"description\""+ lineEnd);
+                        dos.writeBytes(lineEnd);
+                        dos.writeBytes("Description");
+                        dos.writeBytes(lineEnd);
+                        dos.writeBytes(twoHyphens + boundary + lineEnd);
+
+                        dos.writeBytes("Content-Disposition: form-data; name=\"uploadedfile\";filename=\"" + iFileName +"\"" + lineEnd);
+                        dos.writeBytes(lineEnd);
+
+                        Log.e(Tag,"Headers are written");
+
+                        int bytesAvailable = fstrm.available();
+
+                        int maxBufferSize = 1024;
+                        int bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                        byte[ ] buffer = new byte[bufferSize];
+
+                        // read file and write it into form...
+                        int bytesRead = fstrm.read(buffer, 0, bufferSize);
+
+                        while (bytesRead > 0)
+                        {
+                            dos.write(buffer, 0, bufferSize);
+                            bytesAvailable = fstrm.available();
+                            bufferSize = Math.min(bytesAvailable,maxBufferSize);
+                            bytesRead = fstrm.read(buffer, 0,bufferSize);
+                        }
+                        dos.writeBytes(lineEnd);
+                        dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                        // close streams
+                        fstrm.close();
+
+                        dos.flush();
+
+                        Log.e(Tag,"File Sent, Response: "+String.valueOf(conn.getResponseCode()));
+
+                        InputStream is = conn.getInputStream();
+
+                        // retrieve the response from server
+                        int ch;
+                        StringBuffer b =new StringBuffer();
+                        while( ( ch = is.read() ) != -1 ){ b.append( (char)ch ); }
+                        String s=b.toString();
+                        Log.i("Response",s);
+                        dos.close();
 
 
                     }
